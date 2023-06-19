9f319556c5c076785e9406051287608bde240252
Simon <ferlat.simon@gmail.com>
2023-06-19 19:19:37 +0200

    theme switch

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Animation } from "@/components/Animation/Animation"
 import { TitleBlock } from "@/components/TitleBlock/TitleBlock"
+import { About } from "@/components/About/About"
 import { useEffect, useState, useRef } from "react"
 import "./Home.css"
 
@@ -10,6 +11,7 @@ export default function Home() {
   const [screenSplitted, setScreenSplitted] = useState<boolean>(false)
   const [animationIsBlurred, setAnimationBlurred] = useState<boolean>(false)
   const [activeLink, setActiveLink] = useState<String>()
+  const [theme, setTheme] = useState<'auto' | 'dark' | 'light'>('auto')
 
   useEffect(
     () => {
@@ -19,6 +21,44 @@ export default function Home() {
       }, 2000);
 
     }, []
+  )
+
+  useEffect(
+    () => {
+
+      const root = document.documentElement;
+
+
+      function setToDark() {
+        root.style.setProperty('--hero-bg-color', 'rgb(36, 45, 54)');
+        root.style.setProperty('--content-bg-color', 'linear-gradient(to bottom, rgb(83, 88, 92) 3%, #18202c 100%)');
+        root.style.setProperty('--font-color', 'rgb(248, 250, 252)');
+      }
+
+      function setToLight() {
+        root.style.setProperty('--hero-bg-color', 'rgb(248, 250, 252)');
+        root.style.setProperty('--content-bg-color', 'linear-gradient(to bottom, rgba(255, 255, 255, 1) 3%, #8eaad4 100%)');
+        root.style.setProperty('--font-color', 'darkslategray');
+      }
+
+      switch (theme) {
+        case 'auto':
+          if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            setToDark()
+          } else {
+            setToLight()
+          }
+          break;
+        case 'dark':
+          setToDark()
+          break;
+        case 'light':
+          setToLight()
+        default:
+          break;
+      }
+
+    }, [theme]
   )
 
   const handleLinkClick = (link: String) => {
@@ -38,16 +78,22 @@ export default function Home() {
         <div className={`hero ${screenSplitted ? `hero-splitted` : null}`}>
           <Animation isBlurred={animationIsBlurred} />
           <TitleBlock isVisible={titleIsVisible} isSmall={screenSplitted} onLinkClick={handleLinkClick} links={links} activeLink={activeLink ?? ""} />
+          <div className="theme-switcher">
+            <button onClick={() => { setTheme('dark') }}>dark</button>
+            <button onClick={() => { setTheme('light') }}>light</button>
+            <button onClick={() => { setTheme('auto') }}>auto</button>
+          </div>
         </div>
-       <div className={`content ${screenSplitted ? `content-opened` : null }`}>
+        <div className={`content ${screenSplitted ? `content-opened` : null}`}>
           {activeLink === "About" ? (
-            <p>About content</p>
+            <About />
           ) : activeLink === "Projects" ? (
             <p>Projects content</p>
           ) : activeLink === "Contact" ? (
             <p>Contact content</p>
           ) : null}
         </div>
+
       </div>
     </main>
   )
---
+++ components/About/About.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import './About.css'
+import Image from 'next/image'
+
+export function About() {
+
+    const [textIsVisible, setTextIsVisible] = useState(false)
+    const [iconsAreVisible, setIconsAreVisible] = useState(false)
+    const [cvSubtextIsVisible, setCvSubtextIsVisible] = useState(false)
+
+    useEffect(
+        () => {
+            setTextIsVisible(true);
+            setIconsAreVisible(true);
+        }, []
+    )
+
+    return (
+        <div className={`about ${textIsVisible ? `about-visible` : null}`}>
+            <div className='about-content'>
+                <p>I am a passionate web developper. </p>
+                <p>I live in <a target='_blank' className='link link-active' href="https://www.google.com/search?q=Aix-en-Provence">Aix-en-Provence</a>, in the south of France.</p>
+                <p>I currently work for <a target='_blank' href='https://citech.fr/' className='link link-active'>Citech</a>.</p>
+                <p>I specialize in front-end development, specifically with Next.js, React, and Tailwind.</p>
+                <div className={`about-icons ${iconsAreVisible ? "about-iconsVisible" : null}`}>
+                    <Image className='about-icon' src="images/linkedin.svg" alt="linkedin-icon" width="30" height="30" />
+                    <Image className='about-icon' src="images/github.svg" alt="github-icon" width="30" height="30" />
+                    <div className='cv-icon'>
+                        <Image className='about-icon'
+                            src="images/circle-down-solid.svg"
+                            alt="download-cv-icon"
+                            width="30"
+                            height="30"
+                            onMouseEnter={() => setCvSubtextIsVisible(true)}
+                            onMouseLeave={() => setCvSubtextIsVisible(false)}
+                        />
+                        <div className={`about-iconSubtext ${cvSubtextIsVisible ? "about-iconSubtextVisible" : null}`}>Dowload CV</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
