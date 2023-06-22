a91bfc07346b0b4c370d34b84aa2aa9196217713
Simon <ferlat.simon@gmail.com>
2023-06-22 15:39:14 +0200

    about text update

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import Link from 'next/link'
 import './About.css'
 import Image from 'next/image'
 
@@ -18,19 +19,19 @@ export function About() {
     return (
         <div className={`about ${textIsVisible && `about-visible`}`}>
             <div className='about-content'>
-                <p>I am a passionate web developper. </p>
+                <p>I craft web applications. I currently work at <a target='_blank' href='https://citech.fr/' className='link link-active'>Citech</a>. </p>
+                <p>My main expertise lies in front-end development, particularly with React, Next.js, and Tailwind CSS.</p>
                 <p>I live in <a target='_blank' className='link link-active' href="https://www.google.com/search?q=Aix-en-Provence">Aix-en-Provence</a>, in the south of France.</p>
-                <p>I currently work for <a target='_blank' href='https://citech.fr/' className='link link-active'>Citech</a>.</p>
-                <p>I specialize in front-end development, specifically with Next.js, React, and Tailwind.</p>
+                <p>In my free time, I enjoy oil painting. You can find some of my artwork <Link className='link link-active' href="/painting">here</Link> !</p>
                 <div className={`about-icons ${iconsAreVisible && "about-iconsVisible"}`}>
                     <Image className='about-icon' src="images/linkedin.svg" alt="linkedin-icon" width="30" height="30" />
                     <Image className='about-icon' src="images/github.svg" alt="github-icon" width="30" height="30" />
-                        <Image className='about-icon'
-                            src="images/circle-down-solid.svg"
-                            alt="download-cv-icon"
-                            width="30"
-                            height="30"
-                        />
+                    <Image className='about-icon'
+                        src="images/circle-down-solid.svg"
+                        alt="download-cv-icon"
+                        width="30"
+                        height="30"
+                    />
                 </div>
 
             </div>
