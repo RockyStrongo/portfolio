925cf95939ce573968c4d820d7f1542e9bb6f702
Simon <ferlat.simon@gmail.com>
2023-06-24 13:34:47 +0200

    fix icons animation moving text in about page

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -25,7 +25,7 @@ export function About({ firstClick }: AboutProps) {
         <div className={`about ${textIsVisible && `about-visible`}`}>
             <div className='about-content'>
                 <p>I craft web applications. I currently work at <a target='_blank' href='https://citech.fr/' className='link link-active'>Citech</a>. </p>
-                <p>My main expertise lies in front-end development, particularly with React, Next.js, and Tailwind CSS.</p>
+                <p>My main expertise is front-end development, particularly with React, Next.js, and Tailwind CSS.</p>
                 <p>I live in <a target='_blank' className='link link-active' href="https://www.google.com/search?q=Aix-en-Provence">Aix-en-Provence</a>, in the south of France.</p>
                 <p>In my free time, I enjoy oil painting. You can find some of my artwork <Link className='link link-active' href="/painting">here</Link> !</p>
                 <div className={`about-icons ${iconsAreVisible && "about-iconsVisible"}`}>
