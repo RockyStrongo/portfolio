8399c0b3950422db5fa1841b9d21a451b312b7bb
Simon <ferlat.simon@gmail.com>
2023-08-23 14:57:42 +0200

    Content-Security-Policy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+// Before defining your Security Headers
+// add Content Security Policy directives using a template string.
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self';
+  font-src 'self';
+`
+
 const nextConfig = {
     async headers() {
         return [
@@ -8,6 +19,14 @@ const nextConfig = {
                     {
                         key: 'X-Frame-Options',
                         value: 'DENY',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff'
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
                     }
                 ]
             }
